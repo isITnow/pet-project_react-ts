154188645264b2afb827e6469480b866296c729d
Roman Serediuk <box4rom@gmail.com>
2022-10-27 15:07:17 +0300

    add GithubUser type

---
+++ src/types/user.ts
@@ -0,0 +1,15 @@
+export type GithubUser = {
+    login: string,
+    id: number,
+    avatar_url: string,
+    name: string,
+    company: string,
+    blog: string,
+    location: string,
+    bio: string,
+    twitter_username: string,
+    public_repo: string,
+    followers: string,
+    following: string,
+    created_at: string
+}
