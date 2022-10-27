0d41c60ccf492797c972f611917656412b6a7c09
Roman Serediuk <box4rom@gmail.com>
2022-10-27 16:10:03 +0300

    add ThemeSwitcher component

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,9 +1,22 @@
-import styles from './ThemeSwitcher.module.scss';
+import { useState, useEffect } from 'react';
+import { ReactComponent as MoonIcon } from 'assets/icon-moon.svg';
+import { ReactComponent as SunIcon } from 'assets/icon-sun.svg';
 
-interface ThemeSwitcherProps { }
 
-export const ThemeSwitcher = ({ }: ThemeSwitcherProps) => (
-  <div className={styles.themeSwitcher}>
-    ThemeSwitcher Component
+import s from './ThemeSwitcher.module.scss';
+
+
+export const ThemeSwitcher = () => {
+  const [isDark, setIsDark] = useState(false)
+  const themeText = isDark ? 'Dark' : 'Light'
+  const ThemeIcon = isDark ? SunIcon : MoonIcon
+  useEffect(() => {
+    document.body.setAttribute('data-theme', isDark ? 'dark' : 'light')
+  }, [isDark])
+  return (
+    <div className={s.switcher} onClick={()=>setIsDark(!isDark)}>
+      <span>{themeText}</span>
+      <ThemeIcon className={s.icon} />
   </div>
 );
+}
--- templates/default/TemplateName.tsx
+++ templates/default/TemplateName.tsx
@@ -1,9 +1,9 @@
-import styles from './TemplateName.module.scss';
+import s from './TemplateName.module.scss';
 
 interface TemplateNameProps { }
 
 export const TemplateName = ({ }: TemplateNameProps) => (
-  <div className={styles.templateName}>
+  <div className={s.templateName}>
     TemplateName Component
   </div>
 );
